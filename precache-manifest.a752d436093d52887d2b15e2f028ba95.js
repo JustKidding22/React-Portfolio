bb58e89dcaab28dcc1365284344cbb559f23a27c
James K <projdk90@gmail.com>
2022-01-19 13:02:43 -0800

    Updates

---
+++ precache-manifest.a752d436093d52887d2b15e2f028ba95.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997a530d6554199a402d849b3a0111f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "260599942008a3fd70f0",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "24b6eb4d8846d13b6baf",
+    "url": "/static/css/main.e027ecc6.chunk.css"
+  },
+  {
+    "revision": "260599942008a3fd70f0",
+    "url": "/static/js/2.50fa5573.chunk.js"
+  },
+  {
+    "revision": "ae261471e0387de2c27b0b5cf60c4be5",
+    "url": "/static/js/2.50fa5573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b6eb4d8846d13b6baf",
+    "url": "/static/js/main.b5cc6a16.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "13969dcd103308edd836c1fe8d7f1c24",
+    "url": "/static/media/Resume.13969dcd.pdf"
+  },
+  {
+    "revision": "22e4e55d3c37807fcbea8eb224201a3c",
+    "url": "/static/media/profile.22e4e55d.jpeg"
+  },
+  {
+    "revision": "12c4cc210311387cd6a02ff6acda4537",
+    "url": "/static/media/project1.12c4cc21.jpeg"
+  },
+  {
+    "revision": "56066bc7062fbdab36e40b25d319539d",
+    "url": "/static/media/project2.56066bc7.jpeg"
+  },
+  {
+    "revision": "9d55c829dbe75a769033f67a0e23b8d8",
+    "url": "/static/media/project3.9d55c829.jpeg"
+  },
+  {
+    "revision": "6170ee9b852e4bcb9df05453539eafe4",
+    "url": "/static/media/project4.6170ee9b.jpeg"
+  },
+  {
+    "revision": "3fa7e1466dcff4531ab6c5bf086dad0e",
+    "url": "/static/media/project5.3fa7e146.jpeg"
+  },
+  {
+    "revision": "14abeb836166e238927d6013dd13274d",
+    "url": "/static/media/project6.14abeb83.jpeg"
+  }
+]);
