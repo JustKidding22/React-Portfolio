9fed800b63ed917fec78aed9c9a333319305ec47
James K <projdk90@gmail.com>
2022-01-19 15:29:49 -0800

    trbleshooting gh error

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
+
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
