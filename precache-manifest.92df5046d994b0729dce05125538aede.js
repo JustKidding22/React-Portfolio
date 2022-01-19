ab33daa1bad123209c645871287a199fb4d67112
James K <projdk90@gmail.com>
2022-01-19 13:25:17 -0800

    Updates

---
+++ precache-manifest.92df5046d994b0729dce05125538aede.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5234dee4b0668f6ebe010264a0a0531",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "260599942008a3fd70f0",
+    "url": "/react-portfolio/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "24b6eb4d8846d13b6baf",
+    "url": "/react-portfolio/static/css/main.e027ecc6.chunk.css"
+  },
+  {
+    "revision": "260599942008a3fd70f0",
+    "url": "/react-portfolio/static/js/2.50fa5573.chunk.js"
+  },
+  {
+    "revision": "ae261471e0387de2c27b0b5cf60c4be5",
+    "url": "/react-portfolio/static/js/2.50fa5573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b6eb4d8846d13b6baf",
+    "url": "/react-portfolio/static/js/main.b5cc6a16.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "13969dcd103308edd836c1fe8d7f1c24",
+    "url": "/react-portfolio/static/media/Resume.13969dcd.pdf"
+  },
+  {
+    "revision": "22e4e55d3c37807fcbea8eb224201a3c",
+    "url": "/react-portfolio/static/media/profile.22e4e55d.jpeg"
+  },
+  {
+    "revision": "12c4cc210311387cd6a02ff6acda4537",
+    "url": "/react-portfolio/static/media/project1.12c4cc21.jpeg"
+  },
+  {
+    "revision": "56066bc7062fbdab36e40b25d319539d",
+    "url": "/react-portfolio/static/media/project2.56066bc7.jpeg"
+  },
+  {
+    "revision": "9d55c829dbe75a769033f67a0e23b8d8",
+    "url": "/react-portfolio/static/media/project3.9d55c829.jpeg"
+  },
+  {
+    "revision": "6170ee9b852e4bcb9df05453539eafe4",
+    "url": "/react-portfolio/static/media/project4.6170ee9b.jpeg"
+  },
+  {
+    "revision": "3fa7e1466dcff4531ab6c5bf086dad0e",
+    "url": "/react-portfolio/static/media/project5.3fa7e146.jpeg"
+  },
+  {
+    "revision": "14abeb836166e238927d6013dd13274d",
+    "url": "/react-portfolio/static/media/project6.14abeb83.jpeg"
+  }
+]);
